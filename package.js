ce10111f44e6f730edc6beae46e01a8305095199
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-09-03 22:51:27 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "book-server-railway",
+  "version": "1.0.0",
+  "description": "A simple Node.js server to serve book text files, designed for deployment on Railway.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.19.2"
+  }
+}
